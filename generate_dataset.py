f59a4487f9d6510a66451a38de1e7d1a9af44b31
karthikeyan001 <karthikmurugann@gmail.com>
2025-12-08 23:02:39 +0530

    Include fourth data cluster and save as numpy array
    Added a fourth cluster of random data and changed output format.

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,15 +1,14 @@
 import numpy as np
-import pandas as pd
 
 def generate_data():
     np.random.seed(42)
     c1 = np.random.randn(100,2) + [0,0]
     c2 = np.random.randn(100,2) + [5,5]
     c3 = np.random.randn(100,2) + [-5,5]
+    c4 = np.random.randn(100,2) + [5,-5]
 
-    X = np.vstack([c1, c2, c3])
-    df = pd.DataFrame(X, columns=["x","y"])
-    df.to_csv("data.csv", index=False)
+    X = np.vstack([c1, c2, c3, c4])
+    np.save("features.npy", X)
 
 if __name__ == "__main__":
     generate_data()
